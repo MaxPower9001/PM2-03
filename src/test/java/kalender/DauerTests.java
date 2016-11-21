acf7e4cf1aefca872d3e9670728e5780cec11535
Christof <wowcr@gmx.net>
2016-11-21 21:41:58 +0100

    klitzekleiner Test

---
+++ src/test/java/kalender/DauerTests.java
@@ -0,0 +1,24 @@
+package test.java.kalender;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import main.java.kalender.DauerImpl;
+import main.java.kalender.interfaces.Dauer;
+
+public class DauerTests {
+	
+	int dauerInMinuten = 27609;
+
+	@Test
+	public void test() {
+		Dauer dauer = new DauerImpl(dauerInMinuten);
+		assertEquals(2,dauer.anteilWochen());
+		assertEquals(5,dauer.anteilTage());
+		assertEquals(4,dauer.anteilStunden());
+		assertEquals(9,dauer.anteilMinuten());
+	}
+	
+
+}
